c271003f29859998fc74e059f80b004754434379
Florence Martin <florence.martin469@gmail.com>
2021-11-10 11:09:14 +0100

    resolved conflict

--- app.js
+++ app.js
@@ -13,15 +13,14 @@ app.use(express.json());
 app.use(cors());
 
 app.get('/users', (req, res) => {
-  console.log('users/http://localhost:5000/users');
   res.send(users);
 });
 
 app.post('/users', (request, response) => {
   const { name, message } = request.body;
   const newUser = { name, message, id: uniqid() };
-  newUser.push(newUser);
+  users.push(newUser);
   response.send(newUser);
 });
 
-app.listen(5000, () => console.log('server listening on port 5000'));
+app.listen(5001, () => console.log('server listening on port 5001'));
