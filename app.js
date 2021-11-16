1efaec70d801386894991b88a6242eb5ba7776b3
bahstiien <bastien.lecalvez@gmail.com>
2021-11-16 11:10:03 +0100

    add the db

--- app.js
+++ app.js
@@ -1,47 +1,113 @@
 const express = require('express');
-const uniqid = require('uniqid');
-const cors = require('cors');
+const Joi = require('joi');
+const connection = require('./db');
 
-// Place Fake pour faire du contenu
-
-const places = [
-  {
-    id: uniqid(),
-    user: 'Selena',
-    lat: 45.761629,
-    lon: 4.833033,
-    img: 'https://ucarecdn.com/f9ba07c4-b7f2-4077-8840-c51623434a7e/sajadnorixIjpx2dWbu8unsplash.jpg',
-  },
-  {
-    id: uniqid(),
-    name: 'Kalil',
-    lat: 45.76951,
-    lon: 4.867913,
-    img: 'https://ucarecdn.com/d43eae4d-3722-44bb-9a74-c3d9eb658e7c/rafaljedrzejek5LROvubqo4unsplash.jpg',
-  },
-  {
-    id: uniqid(),
-    name: 'Florence',
-    lat: 45.725213,
-    lon: 4.884782,
-    img: 'https://ucarecdn.com/3c74ecb5-4829-4b3f-a5cb-38db425f22c5/dngphchitriuhfDv8pW_uvsunsplash.jpg',
-  },
-  {
-    id: uniqid(),
-    name: 'Bastien',
-    lat: 45.775715,
-    lon: 4.882494,
-    img: 'https://ucarecdn.com/6f125b16-cfdf-40ff-b495-f59eb5393cf6/fernandogagonfGQr5ogRPsunsplash.jpg',
-  },
-];
-
+const serverPort = process.env.PORT || 3000;
 const app = express();
+// const placesFreeRouter = express.Router();
+// app.use('/placesFree', placesFreeRouter);
 
 app.use(express.json());
-app.use(cors());
 
-app.get('/placesFree', (req, res) => {
-  res.send(places);
+connection.connect((err) => {
+  if (err) {
+    console.error('error connecting to db');
+  } else {
+    console.log('connected to db');
+  }
 });
 
-app.listen(3000, () => console.log('server listening on port 3000'));
+placesFreeRouter.get('/placesFree', (req, res) => {
+  connection
+    .promise()
+    .query('SELECT * FROM placesFree')
+    .then(([results]) => {
+      res.json(results);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send('Error retrieving products from db.');
+    });
+});
+
+placesFreeRouter.get('/placesFree/:id', (req, res) => {
+  const { id } = req.params;
+  connection
+    .promise()
+    .query('SELECT * FROM placesFree WHERE id = ?', [id])
+    .then(([results]) => {
+      if (results.length) {
+        res.json(results[0]);
+      } else {
+        res.sendStatus(404);
+      }
+    });
+});
+
+placesFreeRouter.put('/placesFree/:id', (req, res) => {
+  const { error: validationErrors } = Joi.object({
+    user: Joi.string().max(255),
+    lat: Joi.number().min(0),
+    lon: Joi.number().min(0),
+    img: Joi.string().max(255),
+  }).validate(req.body, { abortEarly: false });
+
+  if (validationErrors)
+    return res.status(422).json({ errors: validationErrors.details });
+
+  connection
+    .promise()
+    .query('UPDATE placesFree SET ? WHERE id = ?', [req.body, req.params.id])
+    .then(([result]) => {
+      res.sendStatus(200);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.sendStatus(500);
+    });
+});
+
+placesFreeRouter.post('/placesFree', (req, res) => {
+  const { user, lat, lon, img } = req.body;
+  const { error: validationErrors } = Joi.object({
+    user: Joi.string().max(255).required(),
+    lat: Joi.number().min(0).required(),
+    lon: Joi.number().min(0).required(),
+    img: Joi.string().max(255).required(),
+  }).validate({ user, lat, lon, img }, { abortEarly: false });
+
+  if (validationErrors) {
+    res.status(422).json({ errors: validationErrors.details });
+  } else {
+    connection
+      .promise()
+      .query(
+        'INSERT INTO placesFree (user, lat, lon, img) VALUES (?, ?, ?, ?)',
+        [user, lat, lon, img]
+      )
+      .then(([result]) => {
+        const createdPlaces = { id: result.insertId, user, lat, lon, img };
+        res.json(createdPlaces);
+      })
+      .catch((err) => {
+        console.error(err);
+        res.sendStatus(500);
+      });
+  }
+});
+
+placesFreeRouter.delete('/placesFree/:id', (req, res) => {
+  connection
+    .promise()
+    .query('DELETE FROM placesFree WHERE id = ?', [req.params.id])
+    .then(([result]) => {
+      if (result.affectedRows) res.sendStatus(204);
+      else res.sendStatus(404);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.sendStatus(500);
+    });
+});
+
+app.listen(serverPort);
