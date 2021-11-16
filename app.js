c71014c187db16639cb361ac959f76e6781b9577
bahstiien <bastien.lecalvez@gmail.com>
2021-11-16 11:21:54 +0100

    add the CRUD

--- app.js
+++ app.js
@@ -4,8 +4,8 @@ const connection = require('./db');
 
 const serverPort = process.env.PORT || 3000;
 const app = express();
-// const placesFreeRouter = express.Router();
-// app.use('/placesFree', placesFreeRouter);
+const streetParkingSpotsRouter = express.Router();
+app.use('/streetParkingSpots', streetParkingSpotsRouter);
 
 app.use(express.json());
 
@@ -17,24 +17,33 @@ connection.connect((err) => {
   }
 });
 
-placesFreeRouter.get('/placesFree', (req, res) => {
+streetParkingSpotsRouter.get('/', (req, res) => {
   connection
     .promise()
-    .query('SELECT * FROM placesFree')
+    .query('SELECT * FROM streetParkingSpots')
     .then(([results]) => {
-      res.json(results);
+      res.json(
+        results.map((result) => {
+          return {
+            ...result,
+            lat: parseFloat(result.lat),
+            lon: parseFloat(result.lon),
+          };
+        })
+      );
     })
+
     .catch((err) => {
       console.error(err);
       res.status(500).send('Error retrieving products from db.');
     });
 });
 
-placesFreeRouter.get('/placesFree/:id', (req, res) => {
+streetParkingSpotsRouter.get('/:id', (req, res) => {
   const { id } = req.params;
   connection
     .promise()
-    .query('SELECT * FROM placesFree WHERE id = ?', [id])
+    .query('SELECT * FROM streetParkingSpots WHERE id = ?', [id])
     .then(([results]) => {
       if (results.length) {
         res.json(results[0]);
@@ -44,37 +53,40 @@ placesFreeRouter.get('/placesFree/:id', (req, res) => {
     });
 });
 
-placesFreeRouter.put('/placesFree/:id', (req, res) => {
+// streetParkingSpotsRouter.put('/:id', (req, res) => {
+//   const { error: validationErrors } = Joi.object({
+//     userName: Joi.string().max(255),
+//     lat: Joi.number().min(0),
+//     lon: Joi.number().min(0),
+//     img: Joi.string().max(255),
+//   }).validate(req.body, { abortEarly: false });
+
+//   if (validationErrors)
+//     return res.status(422).json({ errors: validationErrors.details });
+
+//   connection
+//     .promise()
+//     .query('UPDATE streetParkingSpots SET ? WHERE id = ?', [
+//       req.body,
+//       req.params.id,
+//     ])
+//     .then(([result]) => {
+//       res.sendStatus(200);
+//     })
+//     .catch((err) => {
+//       console.error(err);
+//       res.sendStatus(500);
+//     });
+// });
+
+streetParkingSpotsRouter.post('/', (req, res) => {
+  const { userName, lat, lon, img } = req.body;
   const { error: validationErrors } = Joi.object({
-    user: Joi.string().max(255),
-    lat: Joi.number().min(0),
-    lon: Joi.number().min(0),
-    img: Joi.string().max(255),
-  }).validate(req.body, { abortEarly: false });
-
-  if (validationErrors)
-    return res.status(422).json({ errors: validationErrors.details });
-
-  connection
-    .promise()
-    .query('UPDATE placesFree SET ? WHERE id = ?', [req.body, req.params.id])
-    .then(([result]) => {
-      res.sendStatus(200);
-    })
-    .catch((err) => {
-      console.error(err);
-      res.sendStatus(500);
-    });
-});
-
-placesFreeRouter.post('/placesFree', (req, res) => {
-  const { user, lat, lon, img } = req.body;
-  const { error: validationErrors } = Joi.object({
-    user: Joi.string().max(255).required(),
+    userName: Joi.string().max(255).required(),
     lat: Joi.number().min(0).required(),
     lon: Joi.number().min(0).required(),
     img: Joi.string().max(255).required(),
-  }).validate({ user, lat, lon, img }, { abortEarly: false });
+  }).validate({ userName, lat, lon, img }, { abortEarly: false });
 
   if (validationErrors) {
     res.status(422).json({ errors: validationErrors.details });
@@ -82,11 +94,11 @@ placesFreeRouter.post('/placesFree', (req, res) => {
     connection
       .promise()
       .query(
-        'INSERT INTO placesFree (user, lat, lon, img) VALUES (?, ?, ?, ?)',
-        [user, lat, lon, img]
+        'INSERT INTO streetParkingSpots (userName, lat, lon, img) VALUES (?, ?, ?, ?)',
+        [userName, lat, lon, img]
       )
       .then(([result]) => {
-        const createdPlaces = { id: result.insertId, user, lat, lon, img };
+        const createdPlaces = { id: result.insertId, userName, lat, lon, img };
         res.json(createdPlaces);
       })
       .catch((err) => {
@@ -96,18 +108,18 @@ placesFreeRouter.post('/placesFree', (req, res) => {
   }
 });
 
-placesFreeRouter.delete('/placesFree/:id', (req, res) => {
-  connection
-    .promise()
-    .query('DELETE FROM placesFree WHERE id = ?', [req.params.id])
-    .then(([result]) => {
-      if (result.affectedRows) res.sendStatus(204);
-      else res.sendStatus(404);
-    })
-    .catch((err) => {
-      console.error(err);
-      res.sendStatus(500);
-    });
-});
+// streetParkingSpotsRouter.delete('/:id', (req, res) => {
+//   connection
+//     .promise()
+//     .query('DELETE FROM streetParkingSpots WHERE id = ?', [req.params.id])
+//     .then(([result]) => {
+//       if (result.affectedRows) res.sendStatus(204);
+//       else res.sendStatus(404);
+//     })
+//     .catch((err) => {
+//       console.error(err);
+//       res.sendStatus(500);
+//     });
+// });
 
 app.listen(serverPort);
